437b1f868faccffc729f4611e5860741fee854b5
rajabovyours-blip <rajabovyours@gmail.com>
2026-08-17 07:16:11 +0500

    Prepare MilliyMetr for GitHub and iOS build

---
+++ lib/core/api/auth_interceptor.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthInterceptor extends Interceptor {
+  final FlutterSecureStorage _storage;
+
+  AuthInterceptor(this._storage);
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    final token = await _storage.read(key: 'access_token');
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+    handler.next(options);
+  }
+}
