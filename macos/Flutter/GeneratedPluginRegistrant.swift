7336756870a896b4f5c591f06dd0ef935600cd53
rajabovyours-blip <rajabovyours@gmail.com>
2026-08-23 02:39:19 +0500

    Fixes

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,26 @@
 import FlutterMacOS
 import Foundation
 
-import file_picker
+import connectivity_plus
+import file_selector_macos
 import flutter_secure_storage_macos
+import geolocator_apple
+import google_sign_in_ios
+import package_info_plus
 import shared_preferences_foundation
+import sign_in_with_apple
+import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- test_parser.dart
+++ test_parser.dart
@@ -1,9 +1,13 @@
-﻿import 'dart:convert';
-import 'package:milliy_metr/features/home/data/models/banner_model.dart';
-import 'package:milliy_metr/features/categories/data/models/category_model.dart';
-import 'package:milliy_metr/features/products/data/models/product_model.dart';
+﻿import 'package:milliy_metr/features/home/data/models/banner_model.dart';
 
 void main() {
-  final bannerJson = {'id': 'b1', 'imageUrl': 'url', 'linkUrl': 'link', 'title': {'uz':'a','ru':'b','en':'c'}, 'subtitle': {'uz':'a','ru':'b','en':'c'}, 'cta': {'uz':'a','ru':'b','en':'c'}};
+  final bannerJson = {
+    'id': 'b1',
+    'imageUrl': 'url',
+    'linkUrl': 'link',
+    'title': {'uz': 'a', 'ru': 'b', 'en': 'c'},
+    'subtitle': {'uz': 'a', 'ru': 'b', 'en': 'c'},
+    'cta': {'uz': 'a', 'ru': 'b', 'en': 'c'}
+  };
   print(BannerModel.fromJson(bannerJson));
 }
