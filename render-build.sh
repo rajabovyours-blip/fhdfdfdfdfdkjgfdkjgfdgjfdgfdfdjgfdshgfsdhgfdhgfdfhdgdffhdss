4d6f2e4fb93081bc8a6045524909d3e16f16eecb
rajabovyours-blip <rajabovyours@gmail.com>
2026-08-22 15:13:28 +0500

    Configure Render Static Site deployment for admin panel

---
+++ render-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Downloading Flutter SDK..."
+git clone https://github.com/flutter/flutter.git -b stable
+
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Configuring Flutter for Web..."
+flutter config --enable-web
+
+echo "Getting dependencies..."
+flutter pub get
+
+echo "Building Flutter Web application..."
+flutter build web --release --dart-define=API_URL=https://milliy-metr-backend-hdol.onrender.com/api/v1
+
