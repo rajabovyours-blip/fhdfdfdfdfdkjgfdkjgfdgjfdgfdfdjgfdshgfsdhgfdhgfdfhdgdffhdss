973f365a63d58fc1684f06a91b07d5b9d564357a
rajabovyours-blip <rajabovyours@gmail.com>
2026-09-15 02:53:51 +0500

    feat(banners): add mediaType/videoUrl to BannerEntity with isVideo helper

--- lib/features/home/domain/entities/home_entities.dart
+++ lib/features/home/domain/entities/home_entities.dart
@@ -7,6 +7,10 @@ class BannerEntity {
   final LocalizedString title;
   final LocalizedString subtitle;
   final LocalizedString cta;
+  // "image" yoki "video".
+  final String mediaType;
+  // mediaType == "video" bo'lganda videoning server manzili, aks holda bo'sh.
+  final String videoUrl;
 
   BannerEntity({
     required this.id,
@@ -15,5 +19,9 @@ class BannerEntity {
     required this.title,
     required this.subtitle,
     required this.cta,
+    this.mediaType = 'image',
+    this.videoUrl = '',
   });
+
+  bool get isVideo => mediaType == 'video' && videoUrl.isNotEmpty;
 }
